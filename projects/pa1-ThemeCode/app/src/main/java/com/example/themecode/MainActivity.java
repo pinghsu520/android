17be546ac1d9e564ff8add2ac772b6cf302fc6e1
Ping Hsu <pinghsu520@gmail.com>
2019-09-04 01:46:26 -0700

    update

--- projects/pa1-ThemeCode/app/src/main/java/com/example/themecode/MainActivity.java
+++ projects/pa1-ThemeCode/app/src/main/java/com/example/themecode/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.themecode;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-
+import android.view.ViewGroup;
+import android.app.ActionBar;
+import android.app.Activity;
+//import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+
+
+//        ScrollView scrollView = new ScrollView(this);
+//        scrollView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.MATCH_PARENT));
+
+
+//
+//        TextView textView = new TextView(context);
+//        textView.setText("my text");
+//
+//        LinearLayout linearLayout = new LinearLayout(context);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//        linearLayout.addView(textView);
+//        scrollView.addView(linearLayout);
+        TextView textView = new TextView(this);
+        textView.setTextColor(Color.parseColor("#0000ff"));
+        textView.setText("This TextView is dynamically created");
+
+
+
+
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            //Create a layout---------------
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+            //----Create a TextView------
+
+
+//        textView.setLayoutParams(params);
+
+
+
+
+
+
+
+
+
+
+            // show
+
+
+
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        this.addContentView(linearLayout, layoutParams);
+
+
+
+
+
+        linearLayout.addView(textView);
+//        scrollView.addView(linearLayout);
+
     }
 }
