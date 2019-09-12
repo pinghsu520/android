68611f84033be80b7af1d1ea4f88379c8bc71228
Ping Hsu <pinghsu520@gmail.com>
2019-09-11 23:31:19 -0700

    updated the app (list view)

--- projects/pa2-ThemeNav/app/src/main/java/com/example/pa2_themenav/OtherActivity.java
+++ projects/pa2-ThemeNav/app/src/main/java/com/example/pa2_themenav/OtherActivity.java
@@ -16,29 +16,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class OtherActivity extends AppCompatActivity {
-    String[] array = {"Jordan Bell", "Chris Boucher", "Quinn Cook", "Stephen Curry",
-    "Kevin Durant", "Draymond Green", "Andre Iguodala", "Shaun Livingston", "Javale McGee",
-    "Klay Thompson", "David West", "Nick Young"};
-    ArrayList<String> playerList = new ArrayList<String>(Arrays.asList(array));
 
-    // Get a reference to the ListView
-    shoppingListView =
-            (ListView)findViewById(R.id.shopping_list_view);
-
-    // Create a new Array Adapter
-// Specify which layout and view to use for a row
-// and the data (array) to use
-    ArrayAdapter<String> arrayAdapter = new
-            ArrayAdapter<String>(this, R.layout.player_list_row,
-            R.id.player_list_row, playerList);
-
-// Link the ListView and the Adapter
-shoppingListView.setAdapter(arrayAdapter);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+
+
+        ListView playerListView = (ListView) findViewById(R.id.listView);
+
+        String[] array = {"Jordan Bell", "Chris Boucher", "Quinn Cook", "Stephen Curry",
+                "Kevin Durant", "Draymond Green", "Andre Iguodala", "Shaun Livingston", "Javale McGee",
+                "Klay Thompson", "David West", "Nick Young"};
+
+        ArrayList<String> playerList = new ArrayList<String>(Arrays.asList(array));
+        ArrayAdapter<String> arrayAdapter = new
+                ArrayAdapter<String>(this, R.layout.player_list,
+                R.id.player_list_row, playerList);
+
+        playerListView.setAdapter(arrayAdapter);
+
+
+
+
+
     }
 
 
