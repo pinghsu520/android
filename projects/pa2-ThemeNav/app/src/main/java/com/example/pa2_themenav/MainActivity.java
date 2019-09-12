0c349910cc687b0ef540bc33bbd9666d501835fe
Ping Hsu <pinghsu520@gmail.com>
2019-09-11 18:07:49 -0700

    setup pa2

---
+++ projects/pa2-ThemeNav/app/src/main/java/com/example/pa2_themenav/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pa2_themenav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
