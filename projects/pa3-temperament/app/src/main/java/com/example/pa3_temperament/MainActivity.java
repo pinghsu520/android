aa4fc5f4903759c8c32841e3dac6a747848cd316
Ping Hsu <pinghsu520@gmail.com>
2019-09-16 19:18:18 -0700

    update to pa3

--- projects/pa3-temperament/app/src/main/java/com/example/pa3_temperament/MainActivity.java
+++ projects/pa3-temperament/app/src/main/java/com/example/pa3_temperament/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.pa3_temperament;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button secondButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.team_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+
+//        secondButton = (Button) findViewById(R.id.OakArena);
+//        secondButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openActivity1();
+//            }
+//        });
+
+
+
     }
+
+    public void openActivity1() {
+        Intent intent = new Intent(this, OtherActivity.class);
+        startActivity(intent);
+    }
+
+//    public void openActivity2() {
+//        Intent intent = new Intent(this, OaklandArena.class);
+//        startActivity(intent);
+//    }
 }
