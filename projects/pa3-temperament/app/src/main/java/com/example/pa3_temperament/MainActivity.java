d488ef723c7f9d80cf4d87b4511860b9e93891f7
Ping Hsu <pinghsu520@gmail.com>
2019-09-16 18:52:29 -0700

    update

---
+++ projects/pa3-temperament/app/src/main/java/com/example/pa3_temperament/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pa3_temperament;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
