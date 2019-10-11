3949af1f514bde63346451200ace005bb569b2ec
pinghsu520 <pinghsu520@email.arizona.edu>
2019-10-10 23:42:32 -0700

    no action bar

--- projects/pa5-SoundBoard/app/src/main/java/com/example/pa5_soundboard/MainActivity.java
+++ projects/pa5-SoundBoard/app/src/main/java/com/example/pa5_soundboard/MainActivity.java
@@ -9,6 +9,18 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ *
+ * Ping Hsu
+ * CSC 317
+ * Project 5
+ *
+ * The purpose of this program is to implement a soundboard for a topic of my choice which I chose
+ * to have a zoo! The soundboard has 4 animals: wolf, bear, lion, and the elephant. I chose the
+ * tiger as the background because I loved the pictures and I've always had much respect for
+ * lions and tigers beings king/queen of the jungle. The delay button is set to the specifications
+ * with a toggle delay of 0 for red, 1.5sec for yellow, 3 sec for green.
+ */
 public class MainActivity extends AppCompatActivity {
     int count=0;
     Button theTopOne;
@@ -19,11 +31,14 @@ public class MainActivity extends AppCompatActivity {
     int delayTime=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // creates main, sets thread
         mHandler = new Handler();
         theTopOne=(Button) findViewById(R.id.delay);
-        
+
+        // toggle delay button
         theTopOne.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -31,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 blah=0;
                 count++;
                 System.out.println("COUNT: "+count);
+                // changes color+delay times!
                 if(count==1){
                     System.out.println("yellow");
                     delayTime=1500;
@@ -54,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             }
             });
 
-
+    // This button is the first tiger "noise button"
         Button one = (Button) this.findViewById(R.id.button1);
         final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.tiger);
 
@@ -72,12 +88,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    // This button is for the polar bear
 
         Button two = (Button) this.findViewById(R.id.button2);
         final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.polar);
 
         two.setOnClickListener(new View.OnClickListener(){
-
             public void onClick(View v) {
                 if(blah==1) {
                     try {
@@ -91,13 +107,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // This button is for the wolf sound
 
         Button three = (Button) this.findViewById(R.id.button3);
         final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.wolf);
 
         three.setOnClickListener(new View.OnClickListener(){
-
             public void onClick(View v) {
                 if(blah==1) {
                     try {
@@ -111,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // This button is for the elephant
         Button four = (Button) this.findViewById(R.id.button4);
         final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.elephant);
 
