d787759310c384c73c8f1c44dc9bfd2d508d43fb
Ping Hsu <pinghsu520@gmail.com>
2019-10-22 23:40:33 -0700

    final

--- projects/pa6flickr/app/src/main/java/com/example/pa6flickr/MainActivity.java
+++ projects/pa6flickr/app/src/main/java/com/example/pa6flickr/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pa6flickr;
 
-// doinBacground (json) onPostexecute  (json) onAttatchedtoWindwo (from imagefetcher)
+/*
+
+The purpose of this program was to access FLICKR's API through the get Recent call. The interface
+loads two pictures with all meta data information and is important respective to each photo. The interface
+also has 3 buttons: back, reset and next which cycles through new pictures every click!
+
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +14,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,11 +40,21 @@ import java.net.URL;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declares global variables that are called
     public static MainActivity itself = null;
     String url_c1;
     String url_c2;
-    private int i=0;
+    private int i = 0;
     Button goNext;
+    int pageNum = 1;
+    int firstCount=1;
+
+    String owner1;
+    String owner2;
+    String ownerID1;
+    String ownerID2;
+// Initializes main and starts activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,33 +64,67 @@ public class MainActivity extends AppCompatActivity {
         new DownloadTask().execute();
     }
 
-    public void clicked_image(View v){
-        if(v.getId()==R.id.goNext){
-            System.out.println("hi");
+    // When clicking on image, takes it to url. The purpose of this function is to allow the user to
+    // enter a new page
+    public void ClickImage(View V){
+        // First image
+        if(V.getId()==R.id.image1){
+            String url_image="https://flickr.com/photos/"+owner1+"/"+ownerID1;
+            System.out.println(url_image);
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW,  Uri.parse(url_image));
+            startActivity(browserIntent);
+        }
+        else{
+            String url_image2="https://flickr.com/photos/"+owner2+"/"+ownerID2;
+            System.out.println(url_image2);
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse(url_image2));
+            startActivity(browserIntent);
         }
     }
 
+    // This clicks on the next picture and cycles to a new page!
+    public void clicked_next_image(View v){
+            System.out.println(this.pageNum);
+            pageNum++;
+            new DownloadTask().execute();
 
 
+    }
+
+    // This is for the reset button and takes them to the first page
+    public void startAgain(View v){
+        pageNum=1;
+        new DownloadTask().execute();
+    }
+
+    // This clicks on the previous image
+    public void clicked_prev_image(View v){
+        System.out.println(this.pageNum);
+        pageNum--;
+        new DownloadTask().execute();
+
+
+    }
+
+    // The purpose of this function is to call a handler and demonstrates getting hte image URL and
+    // also getting metadata information
     public void onAttachedToWindow(String url_c1, String url_c2) {
 
         final int firstImageID=R.id.image1;
         final int secondImageID=R.id.image2;
 
 
-
+        // handler processes first and second images
         Handler handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 Bitmap image = (Bitmap) msg.obj;
 
                 if(i==1){
-                    System.out.println("FIRSTHIT");
                     ImageView secondimage = findViewById(secondImageID);
                     secondimage.setImageBitmap(image);
                 }
                 else{
-                    System.out.println("SECONDHIT");
                     ImageView firstimage = findViewById(firstImageID);
                     firstimage.setImageBitmap(image);
                     i++;
@@ -83,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        // The purpose of this block is to start an intent and send in urls to the program
         Intent intent = new Intent(this, ImageService.class);
         intent.putExtra("MESSENGER", new Messenger(handler));
         String url1=url_c1;
@@ -95,9 +147,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+    // This private class gets the json information through the URL api.
     private class DownloadTask extends AsyncTask<Object, Void, JSONObject>  {
-        public String newsBaseUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=c68268937007bf1cc8ac4be2d67276b0&extras=url_c%2Cdate_upload%2Ctags&per_page=2&page=1&format=json&nojsoncallback=1";
+        public String newsBaseUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=c68268937007bf1cc8ac4be2d67276b0&extras=url_c%2Cdate_upload%2Ctags&per_page=&page="+Integer.toString(pageNum)+"&format=json&nojsoncallback=1";
 
         @Override
         protected JSONObject doInBackground(Object[] objects) {
@@ -125,8 +177,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
-
+        // The purpose of this function is to call and execute that grabs the infromation needed
+        // from the JSON object. I parsed through the object to grab the meta data.
         @Override
         protected void onPostExecute(JSONObject json) {
 
@@ -135,22 +187,23 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(json);
                 JSONObject jsonObject1=json.getJSONObject("photos");
                 JSONArray array=jsonObject1.getJSONArray("photo");
-//                System.out.println("THIS IS ARR"+array);
+                // grabbed json object
                 JSONObject image1=array.getJSONObject(0);
                 JSONObject image2=array.getJSONObject(1);
                 String dateupload1=image1.getString("dateupload");
                 url_c1=image1.getString("url_c");
-                System.out.println("THIS IS FIRST ULR!!!"+url_c1);
+                // grabbed tag
                 String tags1=image1.getString("tags");
                 String dateupload2=image2.getString("dateupload");
                 url_c2=image2.getString("url_c");
                 String tags2=image2.getString("tags");
-                System.out.println("THIS IS FIRST ULR!!!"+url_c2);
+                // called on attatch to window
                 onAttachedToWindow(url_c1,url_c2);
-
+                String type1=url_c1.substring(url_c1.length() - 3);
+                String type2=url_c2.substring(url_c2.length() - 3);
                 TextView firstdate = (TextView)findViewById(R.id.textView);
                 firstdate.setText(dateupload1);
-
+                // set text blocks
 
                 TextView secondate = (TextView)findViewById(R.id.textView7);
                 secondate.setText(dateupload2);
@@ -162,21 +215,23 @@ public class MainActivity extends AppCompatActivity {
                 TextView firsttag = (TextView)findViewById(R.id.textView3);
                 firsttag.setText(tags1);
 
-
+                // set second text block
                 TextView secondtag = (TextView)findViewById(R.id.textView6);
                 secondtag.setText(tags2);
 
-//
-//                ImageView firstimage = findViewById(R.id.image1);
-//                ImageView secondimage = findViewById(R.id.image2);
-//
-//                TextView setFirstDate=findViewById(R.id.firstdate);
-//                setFirstDate.setText(dateupload1);
-//                TextView setFirstTag=findViewById(R.id.firsttag);
-//                setFirstTag.setText(tags1);
-//
-//                new DownloadImageTask(firstimage).execute(url_c1);
-//                new DownloadImageTask(secondimage).execute(url_c2);
+
+                TextView firstType = (TextView)findViewById(R.id.textView2);
+                firstType.setText(type1);
+
+                TextView secondType = (TextView)findViewById(R.id.textView5);
+                secondType.setText(type2);
+                // info for the images!
+                owner1=image1.getString("owner");
+                owner2=image2.getString("owner");
+
+
+                ownerID1=image1.getString("id");
+                ownerID2=image2.getString("id");
 
 
 
@@ -184,7 +239,6 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-//            getIntent()
 
 
         }
